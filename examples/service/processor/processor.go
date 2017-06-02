d40c44d0eb79a628e2258a55fc317c74dad25b4f
Vadim Petrov <vadim.petrov@gojuno.com>
2017-06-02 12:54:04 +0300

    Presentation slides

--- examples/service/processor/processor.go
+++ examples/service/processor/processor.go
@@ -5,10 +5,9 @@ import ( // OMIT
 	"time"      // OMIT
 ) // OMIT
 // OMIT
-type (
-	Batch []string
-	Fake  struct{}
-)
+type Batch []string // HL
+
+type Fake struct{} // HL
 
 func (f *Fake) Process(batch Batch) {
 	if len(batch) == 0 {
